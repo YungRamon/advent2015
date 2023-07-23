db5781622e9e8a0d99afcf1fd9816034788c4d93
Anton Kartashov <tohakartasov@gmail.com>
2023-07-23 21:49:05 +0200

    Day 0
    Inital Project
    
    Program that will grow to execute various days

---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+printf "Day(yet none): "
+let input = Console.ReadLine()
+
+match input with
+    | _ -> printfn "Wrong Input"
+
+Console.ReadKey() |> ignore
