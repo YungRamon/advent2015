04fde149affeef59035f927b4c2cd289dbae9be3
YungRamon <tohakartasov@gmail.com>
2023-07-30 15:39:01 +0200

    Day 1 solutions

--- Program.fs
+++ Program.fs
@@ -1,9 +1,29 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
-printf "Day(yet none): "
+open System.IO
+printf "Day(1_1,1_2): "
 let input = Console.ReadLine()
 
+let readInput(day:int32,env:string) : string[]= File.ReadAllLines("../../../input/day"+day.ToString()+"-"+env+".txt")
+
+let day1_1 ()= 
+    let input=readInput(1,"game")
+    let count= (input[0] |> Seq.filter (fun xs -> xs='(') |> Seq.length) - (input[0] |> Seq.filter (fun xs -> xs =')') |> Seq.length)
+    Console.WriteLine(count.ToString());
+
+let day1_2 () =
+    let input=readInput(1,"game")
+    let mutable floor= 0;
+    let mutable i=0;
+    while floor <> (-1) do
+        if input[0][i]='(' then floor <- floor+1 else floor <- floor-1
+        i <-i+1
+    Console.WriteLine(i.ToString());
+
+
 match input with
+    | "1_1" -> day1_1()
+    | "1_2" -> day1_2()
     | _ -> printfn "Wrong Input"
 
 Console.ReadKey() |> ignore
