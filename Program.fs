a3080bf174e7367236887da613972931149c7286
YungRamon <tohakartasov@gmail.com>
2023-08-02 18:47:05 +0200

    Day 2

--- Program.fs
+++ Program.fs
@@ -1,7 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
 open System.IO
-printf "Day(1_1,1_2): "
+printf "Day(1_1,1_2,2_1,2_2): "
 let input = Console.ReadLine()
 
 let readInput(day:int32,env:string) : string[]= File.ReadAllLines("../../../input/day"+day.ToString()+"-"+env+".txt")
@@ -20,10 +20,34 @@ let day1_2 () =
         i <-i+1
     Console.WriteLine(i.ToString());
 
+let day2_1 ()= 
+    let input=readInput(2,"game")
+    let mutable total=0
+    for line in input do
+        let dimensionsString=line.Split "x";
+        let dimensions=[|Convert.ToInt32(dimensionsString[0]);Convert.ToInt32(dimensionsString[1]);Convert.ToInt32(dimensionsString[2])|]
+        let sides=[|dimensions[0]*dimensions[1];dimensions[0]*dimensions[2];dimensions[1]*dimensions[2]|]
+        let minimum=sides |> Array.min
+        let size=Seq.fold(fun acc x ->acc+(x*2)) 0 sides
+        total <- (total + size + minimum)
+    Console.WriteLine(total.ToString());
+
+let day2_2 ()= 
+    let input=readInput(2,"game")
+    let mutable total=0
+    for line in input do
+        let dimensionsString=line.Split "x"
+        let dimensions=[|Convert.ToInt32(dimensionsString[0]);Convert.ToInt32(dimensionsString[1]);Convert.ToInt32(dimensionsString[2])|] |> Array.sort
+        let wrap=dimensions[0]+dimensions[0]+dimensions[1]+dimensions[1]
+        let bow=dimensions[0]*dimensions[1]*dimensions[2]
+        total <- (total + wrap + bow )
+    Console.WriteLine(total.ToString());
 
 match input with
     | "1_1" -> day1_1()
     | "1_2" -> day1_2()
+    | "2_1" -> day2_1()
+    | "2_2" -> day2_2()
     | _ -> printfn "Wrong Input"
 
 Console.ReadKey() |> ignore
