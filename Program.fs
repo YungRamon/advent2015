26d78c91e1bf17cd4bafd4d800d4d7bb05eb2932
YungRamon <tohakartasov@gmail.com>
2023-08-06 10:53:12 +0200

    Day 3

--- Program.fs
+++ Program.fs
@@ -1,7 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
 open System.IO
-printf "Day(1_1,1_2,2_1,2_2): "
+printf "Day(1_1,1_2,2_1,2_2,3_1,3_2): "
 let input = Console.ReadLine()
 
 let readInput(day:int32,env:string) : string[]= File.ReadAllLines("../../../input/day"+day.ToString()+"-"+env+".txt")
@@ -43,11 +43,65 @@ let day2_2 ()=
         total <- (total + wrap + bow )
     Console.WriteLine(total.ToString());
 
+//Struct House used in day 3 to define set of coordinates(x,y) for each house visited by Santa
+type House =
+    struct
+        val X: int32
+        val Y: int32
+        new(x: int32, y: int32) = { X = x; Y = y }
+    end
+let day3_1 ()= 
+    let input=readInput(3,"game")
+    let s=seq {
+        let mutable x=0
+        let mutable y=0
+        yield House(x,y)
+        for i in 0..input[0].Length-1 do
+            match input[0][i] with
+            | '>' -> x <-x+1
+            | '<' -> x <-x-1
+            | '^' -> y <-y+1
+            | 'v' -> y <-y-1
+            | _ -> Console.WriteLine("Wrong Input File")
+            yield House(x,y)
+    }
+    let set= Set.ofSeq  s
+    Console.WriteLine(set.Count)
+
+let day3_2 ()= 
+    let input=readInput(3,"game")
+    let s=seq {
+        let mutable x=0
+        let mutable y=0
+        let mutable x'=0
+        let mutable y'=0
+        yield House(x,y)
+        for i in 0..2..input[0].Length-1 do
+            match input[0][i] with
+            | '>' -> x <-x+1
+            | '<' -> x <-x-1
+            | '^' -> y <-y+1
+            | 'v' -> y <-y-1
+            | _ -> Console.WriteLine("Wrong Input File")
+            yield House(x,y)
+            match input[0][i+1] with
+            | '>' -> x' <-x'+1
+            | '<' -> x' <-x'-1
+            | '^' -> y' <-y'+1
+            | 'v' -> y' <-y'-1
+            | _ -> Console.WriteLine("Wrong Input File")
+            yield House(x',y')
+    }
+    let set= Set.ofSeq  s
+    Console.WriteLine(set.Count)
+
 match input with
     | "1_1" -> day1_1()
     | "1_2" -> day1_2()
     | "2_1" -> day2_1()
     | "2_2" -> day2_2()
+    | "3_1" -> day3_1()
+    | "3_2" -> day3_2()
     | _ -> printfn "Wrong Input"
 
 Console.ReadKey() |> ignore
